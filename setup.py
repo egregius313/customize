da80fe3eb4076a7c8dc3d54515b260c28e9ed0ce
egregius313 <egregius313@gmail.com>
2017-07-01 21:08:32 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='customize',
+    version='0.0.1',
+    description='Simple CLI to ease editing .rc files',
+    author='Ed Minnix',
+    author_email='egregius313@gmail.com',
+)
